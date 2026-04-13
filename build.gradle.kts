360a5f1f9dc3f4bfb0a62620c6e60b493fa1cd89
zinuzanu <wsx104301@naver.com>
2026-04-13 18:01:44 +0900

    feat: DB 연동 작업

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
